e0ee098991f291f432972ea1ed1dd5772daa24bb
bxxmi <bmi6638@gmail.com>
2019-12-20 13:06:32 +0900

    프로젝트 다오 이슈부분 추가

--- beepro/src/com/semi/dao/ProjectDao.java
+++ beepro/src/com/semi/dao/ProjectDao.java
@@ -1,7 +1,11 @@
-
 package com.semi.dao;
 
 public interface ProjectDao {
-
+	// 이슈부분
+    String insertIssueSql = "INSERT";
+    String updateIssueSql = "UPDATE ISSUE SET TITLE=?, ISSUE_LEVEL=?, ISSUE_CATEGORY=?, CONTENT=? WHERE ISSUE_SEQ=?";
+    String deleteIssueSql = "DELETE FROM ISSUE WHERE ISSUE_SEQ=?";
+    String selectAllIssueSql = "SELECT * FROM ISSUE ORDER BY ISSUE_SEQ DESC"; 
+    String selectOneIssueSql = "SELECT * FROM ISSUE WHERE ISSUE_SEQ=?";
 }
 
